ef0d9dcdda872416bd6f3612858e85389f2c9658
Keito Koshikawa(越川 慶士) <114968417+keity555@users.noreply.github.com>
2025-09-29 06:37:54 +0000

    fastcomit

---
+++ .devcontainer/post-create-command.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# --- このスクリプトは、コンテナ作成後に一度だけ実行されます ---
+echo "--- Running post-create-command.sh ---"
+
+# --- 実行したいコマンドを追記していきます ---
+
+
+# --- スクリプト終了 ---
+echo "--- Setup completed successfully! ---"
+
+chmod 755 .devcontainer/post-create-command.sh
